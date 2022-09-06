6788814477a2f4c28ed81bb0f25f47ade9eee46b
Colin Chan <colinchan02@gmail.com>
2022-09-05 18:15:10 -0700

    Update list.h

--- list.h
+++ list.h
@@ -10,11 +10,11 @@ struct nodeStruct
 /* Function prototypes */
 
 struct nodeStruct* 	List_createNode(int item);
-void 				List_insertHead(struct nodeStruct **headRef, struct nodeStruct *node);
-void 				List_insertTail(struct nodeStruct **headRef, struct nodeStruct *node);
-int 				List_countNodes(struct nodeStruct *head);
+void 			List_insertHead(struct nodeStruct **headRef, struct nodeStruct *node);
+void 			List_insertTail(struct nodeStruct **headRef, struct nodeStruct *node);
+int 			List_countNodes(struct nodeStruct *head);
 struct nodeStruct* 	List_findNode(struct nodeStruct *head, int item);
-void 				List_deleteNode(struct nodeStruct **headRef, struct nodeStruct* node);
-void 				List_sort(struct nodeStruct **headRef);
+void 			List_deleteNode(struct nodeStruct **headRef, struct nodeStruct* node);
+void 			List_sort(struct nodeStruct **headRef);
 
 #endif
